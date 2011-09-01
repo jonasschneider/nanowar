43ae251d0fa960bd0867b7c28b004abdc332b158
Jonas Schneider <mail@jonasschneider.com>
2011-09-01 19:29:00 +0200

    make yoke smarter

--- yoke.js
+++ yoke.js
@@ -44,13 +44,14 @@ function processFile(file, level, already_parsed) {
   level || (level = 0);
   
   already_parsed || (already_parsed = []);
-  if(already_parsed.indexOf(file) != -1) return "already";
+  if(already_parsed.indexOf(file) != -1) return '';
   already_parsed.push(file);
   
   if (options.verbose) {
     stdio.writeError(pad(level) + '> ' + file + '\n');
   }
-  lines = lines.replace(/^\s*\/\/=\s*require\s+(["<])([^">]+).$/mg, function(match, type, location) {
+  
+  lines = lines.replace(/^\s*(?:\/\/|\#)=\s*require\s+(["<])([^">]+).$/mg, function(match, type, location) {
     var resolved = findOne(location, path.extname(file), (type === '<' ? options.directories : [path.dirname(file)]));
     if (resolved) {
       return processFile(resolved, level + 1, already_parsed);
@@ -58,18 +59,22 @@ function processFile(file, level, already_parsed) {
     throw new Error('Cannot resolve require:\n\n    ' + match + '\n\n in file ' + file);
   });
   
-  lines = lines.replace(/^\s*\/\/=\s*require_tree\s+([^\s]+)$/mg, function(match, dir) {
+  lines = lines.replace(/^\s*(?:\/\/|\#)=\s*require_tree\s+([^\s]+)$/mg, function(match, dir) {
     var matches = glob.globSync(path.join(path.dirname(file), dir, '*'+path.extname(file)))
     
     processed = '';
     
     matches.forEach(function(globbed_file) {
-      if(globbed_file != file)        
-        processed += processFile(globbed_file, level + 1, already_parsed) + "\n\n"
+      if(globbed_file != file) {
+        var processed_globbed = processFile(globbed_file, level + 1, already_parsed)
+        if(processed_globbed.length > 1)
+          processed += processed_globbed + "\n"
+      }
     })
     
     return processed;
   });
+  
   return lines;
 }
 
